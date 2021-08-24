2984ed8ba8928b6985b61f75ba3055444b929c15
Alex Feigin <afeigin@proofpoint.com>
2021-08-24 14:20:10 +0300

    logo change

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <div id="nav">
-    <header>
-      <div id="header-container">
-        <div id="name-in-header">Lior Raz</div>
-        <div id="router-in-header">
-          <router-link to="/">Home</router-link> |
-          <router-link to="/about">About</router-link>
-        </div>
-      </div>
-    </header>
-    <router-view/>
-  </div>
+  <nav>
+    <div>Lior Raz</div>
+    <div>
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </div>
+  </nav>
+  <router-view/>
 </template>
 
 <style>
@@ -24,30 +21,21 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #d1b1c8ff;
 }
 
-#nav {
+nav {
   padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
 }
 
-#nav a {
-  font-weight: bold;
+nav a {
   color: #c7efcfff;
 }
 
-#nav a.router-link-exact-active {
+nav a.router-link-exact-active {
   color: #326771ff;
 }
 
-#header-container{
-  display: flex;
-  justify-content: space-between;
-}
-#name-in-header{
-  text-align: left;
-}
-#router-in-header{
-  text-align: right;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <H1>Welcome to Your Vue.js App!</H1>
+
+    <div id="frame"><img alt="Vue logo" src="../assets/logo.jpeg"></div>
+    <H1>Welcome to my portfolio App!</H1>
   </div>
 </template>
+
+<style>
+#frame img{
+  border-radius: 50%;
+  width: 300px;
+  height: 300px;
+  object-fit: cover;
+}
+</style>
 
 <script>
 // @ is an alias to /src
