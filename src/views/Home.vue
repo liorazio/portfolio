88fb118ffbc57555690e0e7dbe9b28f51aa0f0c7
Alex Feigin <afeigin@proofpoint.com>
2021-08-28 21:18:38 +0300

    fix border for videop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,8 @@
   <div class="home">
     <div class="canvas"><img alt="liorazio logo" src="../assets/logo.gif"></div>
     <div class="canvas reduced-opacity"><img alt="liorazio banner" src="../assets/banner.gif"></div>
-    <div class="relative-container">
-      <img class="frame" alt="frame for video" src="../assets/behind-video.svg">
-      <iframe src="https://www.youtube.com/embed/KoT_5IM2AKM?controls=0"/>
-    </div>
+<!--    <iframe src="https://www.youtube.com/embed/KoT_5IM2AKM"/>-->
+    <iframe src="https://player.vimeo.com/video/444298599?h=30668b83f1" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
     <H1 class="wide-letter-heading">Projects!</H1>
   </div>
 </template>
@@ -16,37 +14,16 @@
   align-content: center;
 }
 
-.relative-container {
-  position: relative;
-  border: solid 5px;
-  height: 400px;
-  width: 80%;
-}
-
 .wide-letter-heading {
   letter-spacing: 0.5em;
   font-weight: bold;
 }
 
-.frame {
-  background-color: transparent;
-  position: absolute;
-  top: 0;
-  left: -200px;
-  /*left: 22.5%;*/
-  /*width: 100%;*/
-  height: 94%;
-  z-index: 0;
-
-}
 
 iframe {
-  position: absolute;
-  height: 86%;
-  top: 10px;
-  left: 24%;
-  z-index: 1;
-  width: 52%;
+  border: solid 15px;
+  width: 45%;
+  height: 25vw;
 }
 
 
@@ -78,10 +55,9 @@ iframe {
   }
 
   iframe {
-    width: 94%;
-    height: 70%;
-    top: 11%;
-    left: 2%;
+    width: 100%;
+    height: 56vw;
+
   }
 }
 
