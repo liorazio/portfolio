d8800af7c64bfac0bffa6c379dbfc5af2f2b29a9
Alex Feigin <afeigin@proofpoint.com>
2021-09-05 00:49:42 +0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# abort on errors
+set -e
+
+# create git ignored working directory to push small delta update on publish
+if [[ ! -d ./gitdist ]]; then
+    git clone git@github.com:liorazio/liorazio.github.io.git gitdist
+fi
+
+# remove old deployment
+find gitdist -maxdepth 1 ! -iname '.git' ! -iname 'gitdist' -print0 | xargs -0 -I{} rm -rf {}
+
+# build
+npm run build
+
+# copy all build files to git working directory
+cp -a ./dist/* ./gitdist/
+
+# create and push deploy commit
+pushd gitdist
+git add .
+git commit -am "deploy $(date '+%d-%b %H:%M')"
+git push origin HEAD
+popd
