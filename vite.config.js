8bd955ef9c8270bd690f55e716278b7cfa777f21
Alex Feigin <alex.feigin@gmail.com>
2024-01-09 13:22:11 +0200

    upgrade dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import {fileURLToPath, URL} from 'node:url'
 
 import {defineConfig, loadEnv} from 'vite'
-
-
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -23,7 +21,6 @@ export default defineConfig(({command, mode}) => {
     return {
         plugins: [htmlPlugin(), vue()],
         resolve: {
-            extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
             alias: {
                 '@': fileURLToPath(new URL('./src', import.meta.url))
             }
